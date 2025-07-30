583268dd1fa3fd4a92a137ce002bc7868bece8d2
enpederson <enpederson@gmail.com>
2025-07-30 14:43:08 -0400

    Delete kmeRNA_eRNA-paRNA_RICseq_example_data/split_train/embeddings/rename.sh

--- kmeRNA_eRNA-paRNA_RICseq_example_data/split_train/embeddings/rename.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mv GSE190214_HeLa.Enhancer_Promoter.chimeric_fragment.out.150nt.EP_anno.uniq.noncoding_seed42813.split_train.DN.enhancers.fa.pairs.RNA.out.tsv.gz \
-	GSE190214_HeLa.Enhancer_Promoter.chimeric_fragment.out.150nt.split_train.DN.enhancer-promoters.tsv.gz
-mv GSE190214_HeLa.Enhancer_Promoter.chimeric_fragment.out.150nt.EP_anno.uniq.noncoding_seed42813.split_train.enhancers.fa.pairs.RNA.out.tsv.gz \
-	GSE190214_HeLa.Enhancer_Promoter.chimeric_fragment.out.150nt.split_train.enhancers.tsv.gz
-mv GSE190214_HeLa.Enhancer_Promoter.chimeric_fragment.out.150nt.EP_anno.uniq.noncoding_seed42813.split_train.promoters.fa.pairs.RNA.out.tsv.gz \
-	GSE190214_HeLa.Enhancer_Promoter.chimeric_fragment.out.150nt.split_train.promoters.tsv.gz
-
